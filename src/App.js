105d78abde8fbc6a4a927f2bcbd5b0ddb0fecca8
Cathrina <cathrina.hao@unity3d.com>
2021-04-11 17:12:24 +0900

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      React Dictionary App
-      <footer>
-        This project was coded by Cathrina Hao and is
-        <a
-          href="https://github.com/cathrinahwj/react-dictionary-project"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Dictionary App</h1>
+        <footer>
+          This project was coded by Cathrina Hao and is
+          <a
+            href="https://github.com/cathrinahwj/react-dictionary-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
