e9a2261af89572c5bb6edeb4f4bf886cf7295043
Cathrina <cathrina.hao@unity3d.com>
2021-04-11 17:01:56 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Dictionary App</div>;
+  return (
+    <div className="App">
+      React Dictionary App
+      <footer>
+        This project was coded by Cathrina Hao and is
+        <a
+          href="https://github.com/cathrinahwj/react-dictionary-project"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
