c8f714ed3065f187d69dcd1b440054be7eec9081
Cathrina <cathrina.hao@unity3d.com>
2021-04-13 19:53:48 +0900

    search engine

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import book from "./book.png";
 import React from "react";
+import Dictionary from "./Dictionary";
 
 import "./App.css";
 
@@ -6,15 +8,22 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Dictionary App</h1>
-        <footer>
-          This project was coded by Cathrina Hao and is
-          <a
-            href="https://github.com/cathrinahwj/react-dictionary-project"
-            target="_blank"
-          >
-            open-sourced on GitHub
-          </a>
+        <header className="App-header">
+          <img src={book} className="App-book" alt="book" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Cathrina Hao and is
+            <a
+              href="https://github.com/cathrinahwj/react-dictionary-project"
+              target="_blank"
+            >
+              open-sourced on GitHub
+            </a>
+          </small>
         </footer>
       </div>
     </div>
